377fc7f6252357800ccc9cfa9bb24fc0234ffdd7
Anna Jaerving <annajrving@gmail.com>
2021-02-22 16:59:41 +0000

    paths

--- src/admin.js
+++ src/admin.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
 
 import { getPage } from './paging.js';
 import { count, deleteRow } from './db.js';
@@ -10,8 +8,6 @@ import { catchErrors, ensureLoggedIn } from './utils.js';
 dotenv.config();
 
 const router = express.Router();
-const path = dirname(fileURLToPath(import.meta.url));
-router.use(express.static(join(path, '../public')));
 
 /**
  * Route handler fyrir admin síðu
